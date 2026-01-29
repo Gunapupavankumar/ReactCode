311a50008253d48ed83180a711d4a37dd2afb8b3
Gunapupavankumar <gunapu1998@gmail.com>
2026-01-29 22:28:44 +0530

    first comment

---
+++ sort.js
@@ -0,0 +1,16 @@
+
+const arr =[1,2,3,[4,5,6],[7,8,9],10];
+
+function flattenArray(array){
+     let result =[];
+     for(let i=0;i<array.length;i++){
+        if(Array.isArray(array[i])){
+            result = result.concat(flattenArray(array[i]));
+        }else {
+            result.push(array[i])
+        }
+     }
+     return result
+}
+
+console.log(flattenArray(arr));
